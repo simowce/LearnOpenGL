7a3b3d2c4791d89e7500f8bac57813e367883028
simowce <simowce@gmail.com>
2022-01-26 16:58:14 +0800

    first commit: Hello OpenGL
    Init project structure and CMakeLists.txt
    
    Signed-off-by: simowce <simowce@gmail.com>

---
+++ build/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/src/glad.c.o"
+  "CMakeFiles/opengl.dir/src/main.cpp.o"
+  "opengl"
+  "opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
